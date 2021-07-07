f16168743c2635c61e1768170beaacf95319ac6e
zNairy <znishikkinishio@gmail.com>
2021-07-06 23:14:08 -0300

    modificado nome de usuario dc

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 __author__ = '@zNairy'
-__contact__ = 'Discord: __Nairy__#7181 | Github: https://github.com/zNairy/'
+__contact__ = 'Discord: zNairy#7181 | Github: https://github.com/zNairy/'
 __version__ = '2.0'
 
 from parserArguments import createSetupParser
